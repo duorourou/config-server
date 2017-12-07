dc251d238093c2bf0c748f364573e1475040591a
neo.xiaoc <neo.xiaoc@gmail.com>
2017-12-07 14:23:13 +0800

    a spring-cloud-config server demo

---
+++ src/main/java/com/thoughtworks/spring/config/demo/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.spring.config.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableAutoConfiguration
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
